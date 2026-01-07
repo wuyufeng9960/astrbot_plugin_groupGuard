8fa3483bfd6316b83bc9c17db30eaecea3b79898
wuyufeng9960 <wyf091020@icloud.com>
2026-01-08 00:43:20 +0800

    fix

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class MyPlugin(Star):
                     except Exception as e:
                         continue
                     logger.info(f"用户 {user_id} 已在目标群 {target_group} 中，将拒绝加群申请")
-                    await client.api.call_action('set_group_add_request', flag=get_value(raw_message, 'flag'), sub_type='add', approve=False, reason=f"您已在群 {target_group} 中，请不要重复加群。")
+                    await client.api.call_action('set_group_add_request', flag=get_value(raw_message, 'flag'), approve=False, reason=f"您已在群 {target_group} 中，请不要重复加群。")
                     return None
                 logger.info(f"用户 {user_id} 不在任何目标群中，将跳过处理")
             except Exception as e:
