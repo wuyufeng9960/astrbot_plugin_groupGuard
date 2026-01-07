a6e7736d30102d87ebe793ecc08e8bcbdaa8483c
wuyufeng9960 <wyf091020@icloud.com>
2026-01-08 00:29:38 +0800

    change detect mode

--- main.py
+++ main.py
@@ -49,10 +49,7 @@ class MyPlugin(Star):
             try:
                 for target_group in self.detect_groups:
                     member_status = await client.api.call_action('get_group_member_info', group_id=int(target_group), user_id=int(user_id))
-                    rtcode = -1
-                    rtcode = member_status.get('retcode', rtcode)
-                    logger.debug(f"检查用户 {user_id} 在目标群 {target_group} 中的状态，返回码: {rtcode}")
-                    if rtcode == 0:
+                    if member_status == None:
                         logger.info(f"用户 {user_id} 已在目标群 {target_group} 中，将拒绝加群申请")
                         await client.api.call_action('set_group_add_request', flag=get_value(raw_message, 'flag'), sub_type='add', approve=False, reason=f"您已在群 {target_group} 中，请不要重复加群。")
                         return None
