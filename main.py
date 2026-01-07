8ee79e49f5f5f07a5236af774ab5d5287a8bb70c
wuyufeng9960 <wyf091020@icloud.com>
2026-01-07 23:47:50 +0800

    添加申请处理程序

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
+import asyncio
+import time
+import traceback
+import json
+from pathlib import Path
+from typing import List, Dict
+import aiohttp
+import os
+import shutil
+
 from astrbot.api import logger
+from astrbot.api.star import StarTools
+from astrbot.api import message_components as Comp
+from astrbot.api.star import Context, Star, register
+from astrbot.core.message.message_event_result import MessageChain
+from astrbot.api.platform import MessageType
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.core.star.filter.platform_adapter_type import PlatformAdapterType
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: dict = None):
         super().__init__(context)
+        self.monitor_groups = [str(g) for g in config.get("monitor_groups", []) or []]
+        self.detect_groups = [str(g) for g in config.get("target_groups", []) or []]
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+    def get_value(obj, key, default=None):
+        try:
+            if isinstance(obj, dict):
+                return obj.get(key, default)
+            return getattr(obj, key, default)
+        except Exception:
+            return default
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.platform_adapter_type(PlatformAdapterType.AIOCQHTTP)
+    @filter.event_message_type(filter.EventMessageType.ALL, priority=10)
+    async def groupin(self, event: AstrMessageEvent):
+        """处理加群申请"""
+        raw_message = event.message_obj.raw_message
+        post_type = get_value(raw_message, "post_type")
+        if post_type == "request" and get_value(raw_message, "request_type") == "group":
+            group_id = get_value(raw_message, "group_id")
+            user_id = get_value(raw_message, "user_id")
+            if str(group_id) not in self.monitor_groups: return none
+            logger.debug(f"收到用户 {user_id} 加群 {group_id} 的申请, 开始检查是否允许通过")
+            # 检查申请人是否在目标群中
+            client = event.bot
+            for target_group in self.detect_groups:
+                member_status = await client.api.call_action('get_group_member_info', group_id=int(target_group), user_id=int(user_id))
+                retcode = member_status.get('retcode', retcode)
+                if retcode == 0:
+                    logger.info(f"用户 {user_id} 已在目标群 {target_group} 中，将拒绝加群申请")
+                    await client.api.call_action('set_group_add_request', flag=get_value(raw_message, 'flag'), sub_type='add', approve=False, reason=f"您已在群 {target_group} 中，请不要重复加群。")
+                    return None
+            logger.info(f"用户 {user_id} 不在任何目标群中，将跳过处理")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
